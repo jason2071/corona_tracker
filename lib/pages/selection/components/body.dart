4d5799348b814a0520e190e6c9837be5bade1f98
Phumai <mcc3939@gmail.com>
2021-01-05 10:41:42 +0700

    add flag on search

--- lib/pages/selection/components/body.dart
+++ lib/pages/selection/components/body.dart
@@ -36,9 +36,44 @@ class _BodyState extends State<Body> {
             return CountryService.getSuggestions2(pattern);
           },
           itemBuilder: (context, CountriesArgument suggestion) {
-            return ListTile(
-              tileColor: Colors.white,
-              title: Text(suggestion.country),
+            return Container(
+              color: Colors.white,
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.network(
+                        suggestion.countryInfo.flag,
+                        width: 40,
+                        height: 40,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    suggestion.country,
+                    style: TextStyle(fontSize: 16),
+                  )
+                ],
+              ),
+            );
+          },
+          noItemsFoundBuilder: (context) {
+            return Container(
+              color: Colors.white,
+              height: 50,
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Text(
+                "No Items Found!",
+                style: TextStyle(
+                  color: Theme.of(context).errorColor,
+                  fontSize: 18,
+                ),
+              ),
             );
           },
           transitionBuilder: (context, suggestionsBox, controller) {
